292212232523436121ade3e4fc808b79f1c85342
theo <brossiertheo@live.fr>
2023-05-15 19:02:39 +0000

    added jsdoc

--- useRecordFields/useRecordFields.js
+++ useRecordFields/useRecordFields.js
@@ -1,36 +1,38 @@
 import {wire} from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-/** @typedef {import('../types/Quote__c').Quote__c} Quote__c*/
 
+/**
+ * @template T
+ * @typedef {new (...args: any[]) => T} GenericConstructor
+ */
 
-const placeholderClass = (genericConstructor, {fields}) => {
-  const {objectApiName} = fields[0]
+/**
+ * @typedef {Object} Field
+ * @prop {string} fieldApiName
+ * @prop {string} objectApiName
+ */
+
+/**
+ * @template T
+ * @param {GenericConstructor<T>} genericConstructor 
+ * @param {Array<Fields>} fields 
+ * @returns {GenericConstructor<Placeholder>}
+ */
+export function useRecordFields(genericConstructor, fields) {
+  const {objectApiName} = fields[0];
+
   class placeholder extends genericConstructor {
     @wire(getRecord, {recordId: '$recordId', fields: fields})
-    _fields
+    _fields;
   }
+
   Object.defineProperty(placeholder.prototype, objectApiName, {
       get() {
         return Object.fromEntries(fields.map((field) => {
-          return [field.fieldApiName, getFieldValue(this._fields.data, field)]
+          return [field.fieldApiName, getFieldValue(this._fields.data, field)];
         }))
       }
   });
-  
-  return [objectApiName, placeholder];
-}
 
-export function useRecordFields(genericConstructor, {recordId, fields}) {
-    const returnableClass = placeholderClass(genericConstructor, {recordId, fields})[1];
-
-    // const m = recordFields.map(recordField => placeholderClass(genericConstructor, recordField))
-    // console.log('map class',m)
-    // for(const [SObject, _class] of m) {
-    //   Object.defineProperty(returnableClass.prototype, SObject, {
-    //     get() {
-    //       return _class.prototype[SObject]
-    //   }})
-    // }
-    console.log('return', returnableClass)
-    return returnableClass;
+  return returnableClass;
 }
