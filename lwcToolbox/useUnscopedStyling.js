afaf8b7757bb1586e62bb489f8efe25e37c901f5
br.Theo <brossiertheo@live.fr>
2023-08-06 19:27:11 +0200

    updated docs

--- lwcToolbox/useUnscopedStyling.js
+++ lwcToolbox/useUnscopedStyling.js
@@ -1,5 +1,23 @@
 import { setDataStyle } from "./utils";
 
+
+/**
+ * Will plug an external stylesheet to an element that has an attribute [data-style]
+ * @example <caption>Basic usage</caption>
+ * // lwc.html
+ * <div data-style></div>
+ * ...rest of my component
+ * // lwc.js
+ * import {useUnscopedStyling} from 'c/lwcToolbox';
+ * const styles = `
+ *   .toastMessage.forceActionsText{
+ *     white-space : pre-line !important;
+ *   }
+ * `;
+ * export default class lwc extends useUnscopedStyling(LightningElement, styles) {...}
+ * @param {GenericConstructor} genericConstructor 
+ * @param {string} styles
+ */
 export function useUnscopedStyling(genericConstructor, styles) {
   return class extends genericConstructor {
     #__isInit__ = false;
