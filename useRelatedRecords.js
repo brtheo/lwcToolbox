ceacc0e9c61de57caf1f7577e9f3d981280b7a36
br.Theo <brossiertheo@live.fr>
2023-11-29 09:04:46 +0100

    useRelatedRecords returns an array and updated args

--- useRelatedRecords.js
+++ useRelatedRecords.js
@@ -16,33 +16,37 @@ import {  wire } from 'lwc';
  * @param {GenericConstructor<T>} genericConstructor 
  * @returns {GenericConstructor<Anonymous>}
  */
-export const useRelatedRecords = (genericConstructor, {relatedListId, fields}) => {
+export const useRelatedRecords = (genericConstructor, {relatedListId, fields, where, sortBy, pageSize}) => {
   const FIELDS_APINAME = fields.map(field => field.split('.').toSpliced(0,1).join('.'))
   const placeholder = class extends genericConstructor {
     @wire(getRelatedListRecords, {
       parentRecordId: '$parentRecordId',
-      relatedListId: relatedListId,
-      fields: fields
+      relatedListId,
+      fields,
+      sortBy,
+      pageSize,
+      where,
     })
     __WIRED_RELATED_RESULTS__;
   }
   Object.defineProperty(placeholder.prototype, relatedListId, {
     get() {
-      let fields;
       if(this?.__WIRED_RELATED_RESULTS__) {
-        fields = this?.__WIRED_RELATED_RESULTS__.data?.records?.at(0)?.fields
-        return Object.fromEntries(
-          FIELDS_APINAME.map(field => {
-            const [related, fieldValue] = field.split('.').at(0).at(-1) === 'r'
-              ? field.split('.')
-              : [field, '']
-            const fieldApiName = fields?.[related]
-            return [
-              related, field.split('.').at(0).at(-1) === 'r'
-                ? Object.fromEntries([[fieldValue, fieldApiName?.value?.fields?.[fieldValue]?.value]])
-                : fieldApiName?.value
-              ]
-          })
+        // fields = this?.__WIRED_RELATED_RESULTS__.data?.records?.at(0)?.fields;
+        return this?.__WIRED_RELATED_RESULTS__.data?.records.map(record => 
+          Object.fromEntries(
+            FIELDS_APINAME.map(field => {
+              const [related, fieldValue] = field.split('.').at(0).at(-1) === 'r' || 's'
+                ? field.split('.')
+                : [field, '']
+              const fieldApiName = record?.[related]
+              return [
+                related, field.split('.').at(0).at(-1) === 'r' || 's'
+                  ? Object.fromEntries([[fieldValue, fieldApiName?.value?.fields?.[fieldValue]?.value]])
+                  : fieldApiName?.value
+                ]
+            })
+          )
         )
       }    
       return undefined;
