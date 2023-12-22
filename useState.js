3d069f448fd5d080451f9f495f5bfa117333e4b0
theo <brossiertheo@live.fr>
2023-12-22 11:53:18 +0100

    Update useState.js

--- useState.js
+++ useState.js
@@ -1,9 +1,11 @@
 import { track } from 'lwc';
 export function useState(genericConstructor, states) {
+  const r = Object.entries(states).find(([k,v]) => v && [k,v] )
+  const [k] = r ? r : [undefined]
   const clazz = class {
-    @track STATE = undefined;
+    @track STATE = k;
   } 
-  Object.defineProperties(clazz.prototype, Object.fromEntries(states.map(state => [
+  Object.defineProperties(clazz.prototype, Object.fromEntries(Object.keys(states).map(state => [
     state, {
       get() { return this.STATE === state; },
       set(toBeSet) { this.STATE = toBeSet ? state : undefined; }
